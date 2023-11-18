82633c857e18f65741971c2946ea6a1dd2c05951
Christophe De Troyer <christophe.detroyer@gmail.com>
2023-11-18 19:23:58 +0100

    wip commit

--- main/consumption.c
+++ main/consumption.c
@@ -7,6 +7,7 @@
 #include "freertos/queue.h"
 #include "freertos/semphr.h"
 #include "freertos/task.h"
+#include <string.h>
 
 // own code
 #include "consumption.h"
@@ -17,18 +18,21 @@ SemaphoreHandle_t stateLock;
 
 #define TAG "CONSUMPTION"
 
-void lock_state()
+void lock_state(char *name)
 {
+
     // grab the lock to update the state
     while (xSemaphoreTake(stateLock, portMAX_DELAY) != pdTRUE) {
         vTaskDelay(pdMS_TO_TICKS(1));
     }
+    // ESP_LOGI(TAG, "lock acquired %s", name);
 }
 
-void unlock_state()
+void unlock_state(char *name)
 {
     // unlock the state
     xSemaphoreGive(stateLock);
+    // ESP_LOGI(TAG, "lock released %s", name);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -37,7 +41,7 @@ void unlock_state()
 void set_sun_percentage(int percentage)
 {
     // grab the lock to update the state
-    lock_state();
+    lock_state("set sun");
 
     if (percentage < 0) {
         percentage = 0;
@@ -46,7 +50,7 @@ void set_sun_percentage(int percentage)
         percentage = 100;
     }
     consumptionState.sunPercentage = percentage;
-    unlock_state();
+    unlock_state("set sun");
 }
 
 int get_sun_production()
@@ -61,44 +65,45 @@ int get_sun_production()
 
 void turn_off(enum Appliance appliance)
 {
-    lock_state();
-    for (int i = 0; i < sizeof(consumptionState.appliances); i++) {
+    lock_state("turn off");
+    for (int i = 0; i < (sizeof(consumptionState.appliances) / sizeof(enum Appliance)); i++) {
         if (consumptionState.appliances[i] == appliance) {
             consumptionState.appliances[i] = NoAppliance;
-            return;
         }
     }
-    unlock_state();
+    unlock_state("turn off");
 }
 
 void turn_on(enum Appliance appliance)
 {
-    lock_state();
+    lock_state("turn on");
     // if the device is present, ignore.
-    for (int i = 0; i < sizeof(consumptionState.appliances); i++) {
+    for (int i = 0; i < (sizeof(consumptionState.appliances) / sizeof(enum Appliance)); i++) {
         if (consumptionState.appliances[i] == appliance) {
-            return;
+            goto unlock;
         }
     }
     // add the appliance in the first empty slot
-    for (int i = 0; i < sizeof(consumptionState.appliances); i++) {
+    for (int i = 0; i < (sizeof(consumptionState.appliances) / sizeof(enum Appliance)); i++) {
         if (consumptionState.appliances[i] == NoAppliance) {
             consumptionState.appliances[i] = appliance;
-            return;
+            goto unlock;
         }
     }
-    unlock_state();
+
+unlock:
+    unlock_state("turn on");
 }
 
 int appliance_consumption(enum Appliance appliance)
 {
     switch (appliance) {
     case Car:
-        return 9000;
+        return 1;
     case Dishwasher:
-        return 1500;
+        return 2;
     case Oven:
-        return 3400;
+        return 4;
     default:
         return 0;
     }
@@ -106,15 +111,52 @@ int appliance_consumption(enum Appliance appliance)
 
 int total_consumption()
 {
-    lock_state();
+    lock_state("total consumption");
     int total = 0;
-    for (int i = 0; i < sizeof(consumptionState.appliances); i++) {
+    int count = (sizeof(consumptionState.appliances) / sizeof(enum Appliance));
+
+    for (int i = 0; i < count; i++) {
         int appliance = appliance_consumption(consumptionState.appliances[i]);
+
         total = total + appliance;
     }
 
-    unlock_state();
+    unlock_state("total consumption");
     return total;
+}
+
+void print_appliance_name(enum Appliance appliace)
+{
+    switch (appliace) {
+    case Car:
+        ESP_LOGI(TAG, "Car");
+        break;
+    case Dishwasher:
+        ESP_LOGI(TAG, "Dishwasher");
+        break;
+    case Oven:
+        ESP_LOGI(TAG, "Oven");
+        break;
+    case Dryer:
+        ESP_LOGI(TAG, "Dryer");
+        break;
+    default:
+        ESP_LOGI(TAG, "NoAppliance");
+        break;
+    }
+}
+
+void print_state()
+{
+    lock_state("print state");
+    ESP_LOGI(TAG, "state start -------------------------");
+    ESP_LOGI(TAG, "consumption: %d", consumptionState.consumption);
+    ESP_LOGI(TAG, "sun: %d", consumptionState.sunPercentage);
+    for (int i = 0; i < (sizeof(consumptionState.appliances) / sizeof(enum Appliance)); i++) {
+        print_appliance_name(consumptionState.appliances[i]);
+    }
+    unlock_state("print state");
+    ESP_LOGI(TAG, "state end ---------------------------");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -127,12 +169,23 @@ void vConsumptionGenerator(void *params)
         vTaskDelay(pdMS_TO_TICKS(1000));
         int consumption = total_consumption();
         ESP_LOGI(TAG, "current consumption: %d", consumption);
+        print_state();
     }
 
     vTaskDelete(NULL);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+void init_state()
+{
+    lock_state("init");
+    memset(&consumptionState, 0, sizeof(ConsumptionState));
+    for (int i = 0; i < (sizeof(consumptionState.appliances) / sizeof(enum Appliance)); i++) {
+        consumptionState.appliances[i] = NoAppliance;
+    }
+    unlock_state("init");
+}
 
 void consumption_set(int consumption)
 {
@@ -144,6 +197,9 @@ void initialize_consumption(void)
 {
     stateLock = xSemaphoreCreateMutex();
     consumptionQueue = xQueueCreate(1, sizeof(int));
+
+    init_state();
+
     xTaskCreate(&vConsumptionGenerator, "vConsumptionGenerator", 2048, NULL, 5, NULL);
 
     return;
